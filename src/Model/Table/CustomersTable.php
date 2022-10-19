6eea2a75c12849bf06137c076a7bdc8313ada67e
Mario Rothauer <office@rothauer-it.com>
2022-10-19 19:49:04 +0200

    never show customer dropowns

--- src/Model/Table/CustomersTable.php
+++ src/Model/Table/CustomersTable.php
@@ -591,7 +591,7 @@ class CustomersTable extends AppTable
         return $creditBalance + 0;
     }
 
-    public function getForDropdown($includeManufacturers = false, $includeOfflineCustomers = true, $conditions = [], $anonymize = false)
+    public function getForDropdown($includeManufacturers = false, $includeOfflineCustomers = true, $conditions = [])
     {
 
         $contain = [];
@@ -633,11 +633,7 @@ class CustomersTable extends AppTable
         $offlineManufacturers = [];
         $onlineManufacturers = [];
         foreach ($customers as $customer) {
-
             $userNameForDropdown = $customer->name;
-            if ($anonymize) {
-                $userNameForDropdown = Configure::read('app.htmlHelper')->anonymizeCustomerName($userNameForDropdown, $customer->id_customer);
-            }
 
             $manufacturerIncluded = false;
             if ($includeManufacturers) {
