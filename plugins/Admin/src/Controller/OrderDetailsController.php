6c2ca524cd5896a0b6cda647f2b397683bfb3e52
Mario Rothauer <office@rothauer-it.com>
2022-10-20 09:09:21 +0200

    send cancellation email to manufacturer as extra email

--- plugins/Admin/src/Controller/OrderDetailsController.php
+++ plugins/Admin/src/Controller/OrderDetailsController.php
@@ -1804,6 +1804,7 @@ class OrderDetailsController extends AdminAppController
                 'appAuth' => $this->AppAuth,
                 'cancellationReason' => $cancellationReason
             ]);
+            $email->addToQueue();
 
             $emailMessage = ' ' . __d('admin', 'An_email_was_sent_to_{0}.', ['<b>' . $orderDetail->customer->name . '</b>']);
 
@@ -1815,10 +1816,9 @@ class OrderDetailsController extends AdminAppController
                     '<b>' . $orderDetail->customer->name . '</b>',
                     '<b>' . $orderDetail->product->manufacturer->name . '</b>'
                 ]);
-                $email->addCC($orderDetail->product->manufacturer->address_manufacturer->email);
+                $email->setTo($orderDetail->product->manufacturer->address_manufacturer->email);
+                $email->addToQueue();
             }
-
-            $email->addToQueue();
 
             $message .= $emailMessage;
 
